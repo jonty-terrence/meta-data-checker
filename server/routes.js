74be592455fc7c75055cf51e974404cb91ab99d2
Jonty-Buchan <jontyterrence@gmail.com>
2020-09-07 15:56:37 +1200

    Sets up routes for adding new data to file

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,18 @@
 const fs = require('fs')
+const express = require('express')
 
+const router = express.Router()
 
-fs.appendFile('metaData.JSON', newData, (err) => {
-  if (err) throw err
+router.post('/', (req, res) => {
+  const { url, title, description } = req.body
+  const newData = {
+    url: url,
+    title: title,
+    description: description
+  }
+  return fs.appendFile('./metaData.JSON', newData, (err) => {
+    if (err) throw err
+  })
 })
+
+module.exports = router
