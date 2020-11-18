90d072f483e3aaae34560289b0e3b97aa80f6d0e
Jonty-Buchan <jontyterrence@gmail.com>
2020-11-18 17:38:59 +1300

    Allows user to download data as csv

--- client/components/LowerSect.jsx
+++ client/components/LowerSect.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import db from '../firebase'
 
+import jsonexport from 'jsonexport'
+
 const deleteButton = () => {
   deleteUserData()
 }
@@ -10,12 +12,26 @@ function deleteUserData () {
   db.database().ref('metaData').set(null)
 }
 
+function downloadData () {
+  const leadsRef = db.database().ref('metaData')
+
+  leadsRef.on('value', function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      const childData = childSnapshot.val()
+      jsonexport(childData, function (err, csv) {
+        if (err) return console.error(err)
+        console.log(csv)
+      })
+    })
+  })
+}
+
 class LowerSect extends React.Component {
   render () {
     return (
       <div className="lower-sect">
         <button className="delete-button" onClick={() => deleteButton()}>Clear data</button>
-        <button className="download-button">Download data</button>
+        <button className="download-button" onClick={() => downloadData()}>Download data</button>
       </div>
     )
   }
